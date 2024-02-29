660f1cf412932a38d1c184603d9bd3f2a12b7240
euGenius <angloweugenious29@gmail.com>
2024-02-29 17:26:19 +0000

    adding a function to create a new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/*
+ * binary_tree_node - creates a binary tree node
+* @parent: source pointer
+ * @value: value of parent
+
+* Return: pointer to parent
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		new_node = malloc(sizeof(binary_tree_t));
+
+		if (new_node == NULL)
+		{
+			dprintf(2, "Malloc error!");
+			return (NULL);
+		}
+		new_node->parent = NULL;
+		new_node->left = NULL;
+		new_node->right = NULL;
+		new_node->n = value;
+		return (new_node);
+	}
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		dprintf(2, "Malloc error");
+		return (NULL);
+	}
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,8 @@
+#include "binary_trees.h"
+/*
+  binary_tree_insert_left - inserts the new node to the left child of the parent
+  @parent: parent to add child
+  @value: value to insert
+  Return: updated pointer
+  */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
