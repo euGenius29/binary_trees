231ff7bbabb814681f493eaf7b15e88e231a9778
euGenius <angloweugenious29@gmail.com>
2024-03-01 09:43:58 +0000

    fixing memory leaks

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,17 +14,21 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 	new_node = malloc(sizeof(binary_tree_t));
-	if (parent->left == NULL)
-	{
-		new_node->parent = parent;
-		parent->left = new_node;
-		new_node->n = value;
-		return (new_node);
-	}
-	new_node->parent = parent;
-	new_node->left = parent->left;
-	parent->left->parent = new_node;
-	parent->left = new_node;
+	if (!new_node)
+		return (NULL);
+	
 	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (parent->left == NULL)
+		parent->left = new_node;
+	else
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+		parent->left = new_node;
+	}
 	return (new_node);
 }
