a66972f7f290c9cb74b725a7a5a74c44314c0ca8
euGenius <angloweugenious29@gmail.com>
2024-03-01 05:21:13 +0000

    Adding a function to delete a binary tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inserts the new node to the left child of the parent
+ * binary_tree_insert_right - inserts the new node to the right child of the parent
  * @parent: parent to add child
  * @value: value to insert
  * Return: updated pointer
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node, *temp;
 
@@ -15,17 +15,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	temp = parent;
 	new_node = malloc(sizeof(binary_tree_t));
-	if (temp->left == NULL)
+	if (new_node == NULL)
+		return (NULL);
+	if (temp->right == NULL)
 	{
 		new_node->parent = temp;
-		temp->left = new_node;
+		temp->right = new_node;
 		new_node->n = value;
 		return (new_node);
 	}
 	new_node-> parent = temp;
-	new_node->left = temp->left;
-	temp->left->parent = new_node;
-	temp->left = new_node;
+	new_node->right = temp->right;
+	temp->right->parent = new_node;
+	temp->right = new_node;
 	new_node->n = value;
 	return (new_node);
 }
